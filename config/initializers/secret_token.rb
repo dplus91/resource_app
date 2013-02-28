a1c7ba0a75276121e8c5369ae85d9b217bb637d8
unknown <DPlus@DPlus-PC.(none)>
2013-02-28 10:11:28 +0700

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ResourceApp::Application.config.secret_token = '8cc8c851b219086978f5962c48dfb82b2a797af9c197bd894f073f8f4e24e42f1583cb11629c59ab0dc672731f73b5b598dfe6cc51785e62c2495104a85296bd'
